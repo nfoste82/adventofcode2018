b54957d543db64e9bfe47e4999b52b2a6f31aaf1
Nic Foster <nfoster@backflipstudios.com>
2018-12-09 22:46:25 -0700

    Day 9 and 10 solutions.

---
+++ Day10/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            _input = File.ReadAllLines("../../../input.txt");
+            
+            List<(Vector2i pos, Vector2i vel)> dots = new List<(Vector2i pos, Vector2i vel)>();
+            
+            foreach (var line in _input)
+            {
+                var first = line.IndexOf('<') + 1;
+                var last = line.IndexOf('>');
+                var positionParts = line.Substring(first, last - first).Replace(" ", "").Split(',');
+                var position = new Vector2i(int.Parse(positionParts[0]), int.Parse(positionParts[1]));
+
+                first = line.IndexOf('<', last + 1) + 1;
+                last = line.IndexOf('>', last + 1);
+                var velocityParts = line.Substring(first, last - first).Replace(" ", "").Split(',');
+                var velocity = new Vector2i(int.Parse(velocityParts[0]), int.Parse(velocityParts[1]));
+
+                dots.Add(new ValueTuple<Vector2i, Vector2i>(position, velocity));
+            }
+
+            int minX = 0;
+            int maxX = 0;
+            int minY = 0;
+            int maxY = 0;
+            FindCorners(dots, out minX, out maxX, out minY, out maxY);
+            int smallestDimensions = Math.Abs((maxX - minX) * (maxY - minY));
+            
+            for (int i = 0; i < 20000; ++i)
+            {
+                for (int j = 0; j < dots.Count; ++j)
+                {
+                    var dot = dots[j];
+                    var newPos = ApplyVelocity(dot.pos, dot.vel, 1);
+                    dots[j] = (new ValueTuple<Vector2i, Vector2i>(newPos, dot.vel));
+                }
+                
+                minX = 0;
+                maxX = 0;
+                minY = 0;
+                maxY = 0;
+                FindCorners(dots, out minX, out maxX, out minY, out maxY);
+
+                int dimensions = Math.Abs((maxX - minX) * (maxY - minY));
+                if (dimensions < smallestDimensions)
+                {
+                    smallestDimensions = dimensions;
+                    Console.WriteLine("Dimensions total: " + smallestDimensions + " at i: " + i);
+
+                    if (i == 10576)    // This i + 1 is the answer to part 2
+                    {
+                        PrintPoints(dots, minX - 2, maxX + 2, minY - 2, maxY + 2);
+                    }
+                }
+            }
+        }
+
+        private static void FindCorners(List<(Vector2i pos, Vector2i vel)> dots, out int minX, out int maxX, out int minY,
+            out int maxY)
+        {
+            minX = Int32.MaxValue;
+            maxX = Int32.MinValue;
+            minY = Int32.MaxValue;
+            maxY = Int32.MinValue;
+
+            foreach (var dot in dots)
+            {
+                var pos = dot.pos;
+                if (pos.X < minX)
+                {
+                    minX = pos.X;
+                }
+
+                if (pos.X > maxX)
+                {
+                    maxX = pos.X;
+                }
+                
+                if (pos.Y < minY)
+                {
+                    minY = pos.Y;
+                }
+
+                if (pos.Y > maxY)
+                {
+                    maxY = pos.Y;
+                }
+            }
+        }
+
+        private static void PrintPoints(List<(Vector2i pos, Vector2i vel)> dots, int minX, int maxX, int minY, int maxY)
+        {
+            for (int y = minY; y < maxY; ++y)
+            {
+                for (int x = minX; x < maxX; ++x)
+                {
+                    bool found = false;
+                    foreach (var dot in dots)
+                    {
+                        if (dot.pos.X == x && dot.pos.Y == y)
+                        {
+                            Console.Write("X");
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static Vector2i ApplyVelocity(Vector2i position, Vector2i velocity, int seconds)
+        {
+            return new Vector2i(position.X + velocity.X * seconds, position.Y + velocity.Y * seconds);
+        }
+
+        private static string[] _input;
+    }
+
+    public struct Vector2i
+    {
+        public int X;
+        public int Y;
+
+        public Vector2i(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int numPlayers = 439;
+            const int numMarbles = 71307;
+            
+            Part1(numPlayers, numMarbles);
+            Part2(numPlayers, numMarbles * 100);
+        }
+
+        private static void Part1(int numPlayers, int numMarbles)
+        {
+            var marblesLeft = numMarbles;
+
+            var scores = new long[numPlayers];
+            var marbles = new List<int>{0, 1};
+
+            var marbleNumber = 1;
+            int currentIndex = 1;
+
+            while (marblesLeft > 0)
+            {
+                --marblesLeft;
+                ++marbleNumber;
+
+                if ((marbleNumber % 23) == 0)
+                {
+                    int playerIndex = marbleNumber % numPlayers;
+                    scores[playerIndex] += marbleNumber;
+                    
+                    currentIndex = MoveIndex(currentIndex, -7, marbles.Count);
+
+                    scores[playerIndex] += marbles[currentIndex];
+                    
+                    marbles.RemoveAt(currentIndex);
+                }
+                else
+                {
+                    currentIndex = MoveIndex(currentIndex, 2, marbles.Count);
+                    marbles.Insert(currentIndex, marbleNumber);
+                }
+            }
+
+            var max = scores.Max();
+            Console.WriteLine("Answer: " + max);
+        }
+        
+        private static void Part2(int numPlayers, int numMarbles)
+        {
+            var scores = new long[numPlayers]; 
+            var linkedList = new LinkedList<long>(); 
+            var currentNode = linkedList.AddFirst(0);
+
+            for (int marbleNumber = 1; marbleNumber < numMarbles; ++marbleNumber)
+            {
+                if (marbleNumber % 23 == 0)
+                {
+                    int playerIndex = marbleNumber % numPlayers;
+                    scores[playerIndex] += marbleNumber;
+
+                    // Move back 7 nodes
+                    for (var i = 0; i < 7; ++i)
+                    {
+                        currentNode = currentNode.Previous ?? linkedList.Last;
+                    } 
+                    
+                    scores[playerIndex] += currentNode.Value; 
+                    
+                    var remove = currentNode; 
+                    currentNode = remove.Next; 
+                    linkedList.Remove(remove);
+                }
+                else
+                {
+                    currentNode = linkedList.AddAfter(currentNode.Next ?? linkedList.First, marbleNumber);
+                }
+            }
+
+            var answer = scores.Max();
+            Console.WriteLine("Answer: " + answer);
+        }
+
+        private static int MoveIndex(int index, int moveAmount, int size)
+        {
+            index += moveAmount;
+            if (index > size)
+            {
+                index -= size;
+            }
+            else if (index < 0)
+            {
+                index += size;
+            }
+
+            return index;
+        }
+    }
+}
