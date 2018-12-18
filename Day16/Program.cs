9172058d41698e9f450a76990da58b2160a680af
Nic Foster <nfoster@backflipstudios.com>
2018-12-18 00:30:26 -0700

    Solutions for day 16 and 17.

---
+++ Day16/Program.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // Input without test program data
+        string[] lines = File.ReadAllLines("../../../input.txt");
+
+        List<string> codes = Codes().ToList();
+
+        int numThreeMatchesOrMore = 0;
+        for (int i = 0; i < lines.Length; i += 4)
+        {
+            int matches = 0;
+            var beforeRegisters = lines[i].GetStringBetweenChars('[', ']').Replace(" ", "").Split(',').ToVector4i();
+            var instruction = lines[i + 1].Split(' ').ToVector4i();
+            var afterRegisters = lines[i + 2].GetStringBetweenChars('[', ']').Replace(" ", "").Split(',').ToVector4i();
+
+            foreach (var code in codes)
+            {
+                var result = OpCode(code, beforeRegisters, instruction);
+                if (afterRegisters.Equals(result))
+                {
+                    matches++;
+
+                    var key = (code, instruction.OpCode);
+                    if (_codesToInstructionCounts.TryGetValue(key, out var count))
+                    {
+                        ++count;
+                        _codesToInstructionCounts[key] = count;
+                    }
+                    else
+                    {
+                        _codesToInstructionCounts.Add(key, count);
+                    }
+                }
+            }
+
+            if (matches >= 3)
+            {
+                ++numThreeMatchesOrMore;
+            }
+        }
+        
+        Console.WriteLine("Part 1 answer: " + numThreeMatchesOrMore);
+
+        bool removed = true;
+        while (removed)
+        {
+            removed = false;
+
+            for (int i = 0; i < 16; ++i)
+            {
+                // Found a lone code for an instruction, remove all other instances of the code
+                if (_codesToInstructionCounts.Count(kvp => kvp.Key.instructionNum == i) == 1)
+                {
+                    var code = _codesToInstructionCounts.Single(kvp => kvp.Key.instructionNum == i).Key.code;
+                    
+                    var others = _codesToInstructionCounts.Where(pair => pair.Key.code == code || pair.Key.instructionNum == i).ToList();
+                    if (others.Count > 1)
+                    {
+                        removed = true;
+
+                        foreach (var kvp in others)
+                        {
+                            _codesToInstructionCounts.Remove(kvp.Key);
+                        }
+                        _codesToInstructionCounts.Add((code, i), 1);
+                    }
+
+                    if (removed)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        string[] testProgLines = File.ReadAllLines("../../../input_alt.txt");
+
+        Vector4i registers = new Vector4i(0,0,0,0);
+        foreach (var line in testProgLines)
+        {
+            var instruction = line.Split(' ').ToVector4i();
+            registers = OpCode(instruction.OpCode, registers, instruction);
+        }
+        
+        Console.WriteLine("Part 2 answer: " + registers.OpCode);
+    }
+
+    static Dictionary<(string code, int instructionNum), int> _codesToInstructionCounts = new Dictionary<(string code, int instructionNum), int>();    
+
+    public static Vector4i OpCode(int code, Vector4i registers, Vector4i instructions)
+    {
+        return OpCode(_codesToInstructionCounts.Single(kvp => kvp.Key.instructionNum == code).Key.code, registers, instructions);
+    }
+
+    public static Vector4i OpCode(string code, Vector4i registers, Vector4i instructions)
+    {
+        switch (code)
+        {
+            case "addr": return ADDOpCode(false, registers, instructions);
+            case "addi": return ADDOpCode(true, registers, instructions);
+            case "mulr": return MULOpCode(false, registers, instructions);
+            case "muli": return MULOpCode(true, registers, instructions);
+            case "banr": return BitwiseANDOpCode(false, registers, instructions);
+            case "bani": return BitwiseANDOpCode(true, registers, instructions);
+            case "borr": return BitwiseOROpCode(false, registers, instructions);
+            case "bori": return BitwiseOROpCode(true, registers, instructions);
+            case "setr": return AssignmentOpCode(false, registers, instructions);
+            case "seti": return AssignmentOpCode(true, registers, instructions);
+            case "gtir": return GreaterThanOpCode(true, false, registers, instructions);
+            case "gtri": return GreaterThanOpCode(false, true, registers, instructions);
+            case "gtrr": return GreaterThanOpCode(false, false, registers, instructions);
+            case "eqir": return EqualityOpCode(true, false, registers, instructions);
+            case "eqri": return EqualityOpCode(false, true, registers, instructions);
+            case "eqrr": return EqualityOpCode(false, false, registers, instructions);
+            default:
+                throw new ArgumentOutOfRangeException(nameof(code));
+        }
+    }
+
+    public static IEnumerable<string> Codes()
+    {
+        yield return "addr";
+        yield return "addi";
+        yield return "mulr";
+        yield return "muli";
+        yield return "banr";
+        yield return "bani";
+        yield return "borr";
+        yield return "bori";
+        yield return "setr";
+        yield return "seti";
+        yield return "gtir";
+        yield return "gtri";
+        yield return "gtrr";
+        yield return "eqir";
+        yield return "eqri";
+        yield return "eqrr";
+    }
+
+    private static Vector4i ADDOpCode(bool immediate, Vector4i registers, Vector4i instructions)
+    {   
+        // Addition:
+        //
+        // addi (add immediate) stores into register C the result of adding register A and value B.
+        // addr (add register) stores into register C the result of adding register A and register B.
+        
+        int result = registers.Get(instructions.A);
+        result += immediate ? instructions.B : registers.Get(instructions.B);
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        
+        //Console.WriteLine(registersOut);
+        return registersOut;
+    }
+
+    private static Vector4i MULOpCode(bool immediate, Vector4i registers, Vector4i instructions)
+    {
+        // Multiplication:
+
+        // muli (multiply immediate) stores into register C the result of multiplying register A and value B.
+        // mulr (multiply register) stores into register C the result of multiplying register A and register B.
+        
+        int result = registers.Get(instructions.A);
+        result *= immediate ? instructions.B : registers.Get(instructions.B);
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        return registersOut;
+    }
+
+    private static Vector4i BitwiseANDOpCode(bool immediate, Vector4i registers, Vector4i instructions)
+    {
+        // bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+        // banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+
+        int result = registers.Get(instructions.A);
+        result &= immediate ? instructions.B : registers.Get(instructions.B);
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        return registersOut;
+    }
+    
+    private static Vector4i BitwiseOROpCode(bool immediate, Vector4i registers, Vector4i instructions)
+    {
+        // bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+        // banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+
+        int result = registers.Get(instructions.A);
+        result |= immediate ? instructions.B : registers.Get(instructions.B);
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        return registersOut;
+    }
+    
+    private static Vector4i AssignmentOpCode(bool immediate, Vector4i registers, Vector4i instructions)
+    {
+        // seti (set immediate) stores value A into register C. (Input B is ignored.)
+        // setr (set register) copies the contents of register A into register C. (Input B is ignored.)
+
+        int result = immediate ? instructions.A : registers.Get(instructions.A);
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        return registersOut;
+    }
+
+    private static Vector4i GreaterThanOpCode(bool immediateA, bool immediateB, Vector4i registers, Vector4i instructions)
+    {
+        //gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0.
+        //gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0.
+        //gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
+        
+        int a = immediateA ? instructions.A : registers.Get(instructions.A);
+        int b = immediateB ? instructions.B : registers.Get(instructions.B);
+        int result = (a > b) ? 1 : 0; 
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        return registersOut;
+    }
+    
+    private static Vector4i EqualityOpCode(bool immediateA, bool immediateB, Vector4i registers, Vector4i instructions)
+    {
+        // eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0.
+        // eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0.
+        // eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
+        
+        int a = immediateA ? instructions.A : registers.Get(instructions.A);
+        int b = immediateB ? instructions.B : registers.Get(instructions.B);
+        int result = (a == b) ? 1 : 0; 
+        
+        Vector4i registersOut = registers;
+        registersOut.Set(instructions.C, result);
+        return registersOut;
+    }
+}
+    
+    public struct Vector4i
+    {
+        public Vector4i(string[] values)
+        {
+            OpCode = int.Parse(values[0]);
+            A = int.Parse(values[1]);
+            B = int.Parse(values[2]);
+            C = int.Parse(values[3]);
+        }
+        
+        public Vector4i(int opCode, int a, int b, int c)
+        {
+            OpCode = opCode;
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public void Set(int register, int value)
+        {
+            if (register == 0) OpCode = value;
+            if (register == 1) A = value;
+            if (register == 2) B = value;
+            if (register == 3) C = value;
+        }
+
+        public int Get(int register)
+        {
+            if (register == 0) return OpCode;
+            if (register == 1) return A;
+            if (register == 2) return B;
+            if (register == 3) return C;
+
+            throw new ArgumentOutOfRangeException(nameof(register));
+        }
+
+        public bool Equals(Vector4i other)
+        {
+            return OpCode == other.OpCode && A == other.A && B == other.B && C == other.C;
+        }
+
+        public override string ToString()
+        {
+            return $"{OpCode},{A},{B},{C}";
+        }
+
+        public int OpCode, A, B, C;
+    }
+    
+    public static class StringExtensions
+    {
+        public static string GetStringBetweenChars(this string input, char begin, char end)
+        {
+            return input.Split(begin, end)[1];
+        }
+        
+        public static Vector4i ToVector4i(this string[] str)
+        {
+            return new Vector4i(str);
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../input.txt");
+
+            Part1(lines);
+            Part2(lines);
+        }
+
+        private static void Part1(string[] lines)
+        {
+            var gridOrig = new List<string>();
+            foreach (var line in lines)
+            {
+                gridOrig.Add(line);
+            }
+
+            for (int minutes = 1; minutes <= 10; ++minutes)
+            {
+                var grid = new List<string>(gridOrig);
+
+                int width = grid[0].Length;
+                for (int y = 0; y < grid.Count; ++y)
+                {
+                    for (int x = 0; x < width; ++x)
+                    {
+                        char charAtPos = grid[y][x];
+                        if (charAtPos == '.') // Open acre
+                        {
+                            if (NumCharSurrounding('|', x, y, grid) >= 3)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '|');
+                            }
+                        }
+                        else if (charAtPos == '|') // Trees
+                        {
+                            if (NumCharSurrounding('#', x, y, grid) >= 3)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '#');
+                            }
+                        }
+                        else if (charAtPos == '#') // Lumberyard
+                        {
+                            if (NumCharSurrounding('#', x, y, grid) == 0)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '.');
+                                continue;
+                            }
+
+                            if (NumCharSurrounding('|', x, y, grid) == 0)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '.');
+                            }
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException(nameof(charAtPos));
+                        }
+                    }
+                }
+            }
+            
+            int numLumberyards = 0;
+            int numTrees = 0;
+            for (int y = 0; y < gridOrig.Count; ++y)
+            {
+                int width = gridOrig[0].Length;
+                for (int x = 0; x < width; ++x)
+                {
+                    char charAtPos = gridOrig[y][x];
+                        
+                    if (charAtPos == '|') ++numTrees;
+                    if (charAtPos == '#') ++numLumberyards;
+                }
+            }
+                
+            Console.WriteLine("Part 1 answer: " + numTrees * numLumberyards);
+        }
+        
+        private static void Part2(string[] lines)
+        {
+            var gridOrig = new List<string>();
+            foreach (var line in lines)
+            {
+                gridOrig.Add(line);
+            }
+            
+            for (int minutes = 1; minutes < 10200; ++minutes)
+            {
+                var grid = new List<string>(gridOrig);
+
+                int width = grid[0].Length;
+                for (int y = 0; y < grid.Count; ++y)
+                {
+                    for (int x = 0; x < width; ++x)
+                    {
+                        char charAtPos = grid[y][x];
+                        if (charAtPos == '.') // Open acre
+                        {
+                            if (NumCharSurrounding('|', x, y, grid) >= 3)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '|');
+                            }
+                        }
+                        else if (charAtPos == '|') // Trees
+                        {
+                            if (NumCharSurrounding('#', x, y, grid) >= 3)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '#');
+                            }
+                        }
+                        else if (charAtPos == '#') // Lumberyard
+                        {
+                            if (NumCharSurrounding('#', x, y, grid) == 0)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '.');
+                                continue;
+                            }
+
+                            if (NumCharSurrounding('|', x, y, grid) == 0)
+                            {
+                                gridOrig[y] = gridOrig[y].ReplaceAt(x, '.');
+                            }
+                        }
+                        else
+                        {
+                            throw new ArgumentOutOfRangeException(nameof(charAtPos));
+                        }
+                    }
+                }
+
+                // Wait until the forest has become stable and then output its data.
+                // Inspect the data, look for the period length for repetitions.
+                // Then find 1000000000 % period.
+                // Then find a minute number printed out by the Console.WriteLine below with the same module as (1000000000 % period length).
+                // The resource value for that minute is your answer.
+                if (minutes > 10000)
+                {   
+                    int numLumberyards = 0;
+                    int numTrees = 0;
+                    for (int y = 0; y < gridOrig.Count; ++y)
+                    {
+                        for (int x = 0; x < width; ++x)
+                        {
+                            char charAtPos = gridOrig[y][x];
+                        
+                            if (charAtPos == '|') ++numTrees;
+                            else if (charAtPos == '#') ++numLumberyards;
+                        }
+                    }
+                
+                    Console.WriteLine("Minutes: " + minutes + ", Resource value: " + numTrees * numLumberyards);
+                }
+            }
+        }
+
+        private static int NumCharSurrounding(char c, int x, int y, List<string> grid)
+        {
+            int total = 0;
+            
+            if (x > 0 && y > 0 && grid[y-1][x-1] == c) ++total;
+            if (y > 0 && grid[y-1][x] == c) ++total;
+            if ((x < (grid[0].Length - 1)) && y > 0 && grid[y-1][x+1] == c) ++total;
+
+            if (x > 0 && grid[y][x-1] == c) ++total;
+            if ((x < (grid[0].Length - 1)) && grid[y][x+1] == c) ++total;
+            
+            if (x > 0 && (y < grid.Count - 1) && grid[y+1][x-1] == c) ++total;
+            if ((y < grid.Count - 1) && grid[y+1][x] == c) ++total;
+            if ((x < (grid[0].Length - 1)) && (y < grid.Count - 1) && grid[y+1][x+1] == c) ++total;
+
+            return total;
+        }
+    }
+    
+    public static class StringExtensions
+    {
+        public static string ReplaceAt(this string input, int index, char newChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
+    }
+}
